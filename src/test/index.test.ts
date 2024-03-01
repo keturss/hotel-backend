81050f936c96feee179208bc7cd63dee5fcb25f3
Keturss <mattoluge@gmail.com>
2024-03-01 20:52:43 +0100

    First commit

---
+++ src/test/index.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import {App} from '@/app';
+import IndexRoute from '@routes/index.route';
+import mongoose from 'mongoose';
+
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+  await mongoose.connection.close();
+});
+
+describe('Testing Index', () => {
+  describe('[GET] /', () => {
+    it('response statusCode 200', () => {
+      const indexRoute = new IndexRoute();
+      const app = new App([indexRoute]);
+
+      return request(app.getServer()).get(`${indexRoute.path}`).expect(200);
+    });
+  });
+});
